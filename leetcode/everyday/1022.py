80689898c052d0f28dc7a71826688ef793321c71
Currycurrycurry <17302010063@fudan.edu.cn>
2020-10-23 09:10:33 +0800

    update on 1023

---
+++ leetcode/everyday/1022.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        start, end, result = 0, 0, []
+        char_map = [0 for _ in range(26)]
+        for i in range(len(S)):
+            char_map[ord(S[i]) - ord('a')] = i
+        for i in range(len(S)):
+            end = max(end, char_map[ord(S[i]) - ord('a')])
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+        return result
