b8087fdd87a698efb0080fa564f9243501f26df2
Currycurrycurry <17302010063@fudan.edu.cn>
2020-11-10 10:19:19 +0800

    update on 1110

---
+++ leetcode/array/threeConsecutiveOdds.py
@@ -0,0 +1,9 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(len(arr)-2):
+            if arr[i] % 2 == 1 and arr[i+1] % 2 == 1 and arr[i+2] % 2 == 1:
+                return True
+        return False
+
+
+        
