ce6503d5fd42869e1b91f6012d6754bbd9c527c8
Currycurrycurry <17302010063@fudan.edu.cn>
2021-09-15 09:06:05 +0800

    commit 2021.9.15

---
+++ leetcode/everyday/2021.9/0915.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[mid+1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+        
